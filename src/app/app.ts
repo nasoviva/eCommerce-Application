7c699ed9a60960fa428367c3ceed9cc03a5015f5
Anton Smelchakov <myalysum@gmail.com>
2025-05-26 19:12:16 +0300

    feat: category data parser

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
+import type {
+  CategoryPagedQueryResponse,
+  ClientResponse,
+  ProductProjectionPagedSearchResponse,
+} from "@commercetools/platform-sdk";
 import "../style.css";
 import FooterView from "./layout/footer/footer";
 import HeaderView from "./layout/header/header";
 import MainView from "./layout/main/main";
 import ApiRequestService from "./services/api-request-service/api-request-service";
 import StateManager from "./services/state-manager/state-manager";
+import DataParser from "./services/api-request-service/data-parser";
 
 export default class App {
   private readonly mainView: MainView;
@@ -86,10 +92,46 @@ export default class App {
         defaultBillingAddress: 0,
       }); */
       /* Тест логина юзера */
-      this.apiRequestService.authUser({
+      /* this.apiRequestService.authUser({
         email: "testemail@testemail.com",
         password: "Test123!",
-      });
+      }); */
+
+      /* Пример получения категорий */
+      this.apiRequestService.getCategories(
+        (result: ClientResponse<CategoryPagedQueryResponse>) => {
+          console.log(DataParser.parseCategories(result, "en-US"));
+        },
+      );
+      /*Пример получения товаров */
+      this.apiRequestService.getProducts(
+        {
+          locale: "en-US",
+          attributes: {
+            byKey: {
+              "card-color": "blue",
+            },
+            byName: {
+              material: "paper",
+            },
+          },
+          price: {
+            from: 500,
+            to: 1020,
+          },
+          sort: {
+            price: "desc",
+            name: "asc",
+          },
+          categories: [
+            "5494ada2-4552-4264-9d9f-59b836591845",
+            "58d82ca1-34de-417b-b9ec-90a28bd0a043",
+          ],
+        },
+        (result: ClientResponse<ProductProjectionPagedSearchResponse>) => {
+          console.log(DataParser.parseForCatalog(result, "en-US"));
+        },
+      );
     });
   }
 }
